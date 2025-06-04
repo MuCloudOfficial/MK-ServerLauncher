eb4aec623cc8f1f38900c79d435c642e9e87154e
Mu_Cloud <Mu_Cloud@outlook.de>
2025-06-04 16:19:27 +0800

    MuCore(fix): Adjust Gson Deserializer for MuPacket

--- Core/src/main/kotlin/me/mucloud/application/MK/ServerLauncher/internal/protocol/packets/MuPacket.kt
+++ Core/src/main/kotlin/me/mucloud/application/MK/ServerLauncher/internal/protocol/packets/MuPacket.kt
@@ -9,6 +9,8 @@ import com.google.gson.JsonParser
 import com.google.gson.JsonSerializationContext
 import com.google.gson.JsonSerializer
 import java.lang.reflect.Type
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
 
 interface MuPacket {
 
@@ -43,9 +45,16 @@ interface MuPacket {
 
 object MuPacketFactory: JsonSerializer<MuPacket>, JsonDeserializer<MuPacket>{
 
-    private val ReflectPool = mutableMapOf<String, MuPacket>()
+    private val ReflectPool = mutableMapOf<String, Class<out MuPacket>>(
+        "config" to MuServerConfigPacket::class.java,
+        "msg.in" to MuSend2ServerPacket::class.java,
+        "server:info" to MuServerInfoPacket::class.java,
+        "server:start" to MuServerStartPacket::class.java,
+        "server:stop" to MuServerStopPacket::class.java,
+    )
 
-    private fun reflectPacket(type: String): MuPacket = ReflectPool[type] ?: throw UnsupportedOperationException("Unsupported MuPacket Type")
+    private fun reflectPacket(type: String): Class<out MuPacket> =
+        (ReflectPool[type] ?: throw UnsupportedOperationException("Unsupported MuPacket Type"))
 
     override fun serialize(
         m: MuPacket,
@@ -61,7 +70,7 @@ object MuPacketFactory: JsonSerializer<MuPacket>, JsonDeserializer<MuPacket>{
         t: Type,
         c: JsonDeserializationContext
     ): MuPacket =
-        reflectPacket(j.asJsonObject["type"].asString).deserialize(j.asJsonObject["data"].asJsonObject)
+        reflectPacket(j.asJsonObject["type"].asString).kotlin.createInstance().deserialize(j.asJsonObject["data"].asJsonObject)
 
 }
 
