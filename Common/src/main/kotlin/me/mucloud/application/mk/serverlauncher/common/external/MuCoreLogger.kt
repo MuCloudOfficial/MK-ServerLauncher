28f30da1e46e0fb0320fb775d1a0718ed46d504e
Mu_Cloud <Mu_Cloud@outlook.de>
2025-12-04 22:45:16 +0800

    [MuCore]: Replace MuCoreLogger with MuLogger in the unified Logger System

--- Common/src/main/kotlin/me/mucloud/application/mk/serverlauncher/common/external/MuCoreLogger.kt
+++
@@ -1,6 +0,0 @@
-package me.mucloud.application.mk.serverlauncher.common.external
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-val log: Logger = LoggerFactory.getLogger("MK-ServerLauncher | MuCore")
