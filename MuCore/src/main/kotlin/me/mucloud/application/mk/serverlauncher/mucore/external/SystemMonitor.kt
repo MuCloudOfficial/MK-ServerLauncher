844759f659fc2624f876ec5f896948e0bc0d7420
Mu_Cloud <Mu_Cloud@outlook.de>
2026-02-26 15:25:21 +0800

    [MuCore] Fix SystemMonitor: Replace Timer with Kotlin Coroutine

--- MuCore/src/main/kotlin/me/mucloud/application/mk/serverlauncher/mucore/external/SystemMonitor.kt
+++ MuCore/src/main/kotlin/me/mucloud/application/mk/serverlauncher/mucore/external/SystemMonitor.kt
@@ -3,7 +3,8 @@ package me.mucloud.application.mk.serverlauncher.mucore.external
 import com.sun.management.OperatingSystemMXBean
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,13 +13,11 @@ import kotlinx.serialization.Serializable
 import me.mucloud.application.mk.serverlauncher.MuCoreMini
 import me.mucloud.application.mk.serverlauncher.muserver.ServerPool
 import java.lang.management.ManagementFactory
-import java.util.*
-import kotlin.concurrent.schedule
 
 object SystemMonitor{
 
-    private var Interval: Int = MuCoreMini.getMuCoreConfig().getSystemMonitorInterval() // Seconds
-    private lateinit var Tsk: TimerTask
+    private var Interval: Int = MuCoreMini.getMuCoreConfig().systemMonitorInterval // Seconds
+    private var isActive: Boolean = false
     private var MonitorFlow: MutableStateFlow<StatusPacket> = MutableStateFlow(getCurrentStatus())
 
     private fun getCurrentStatus(): StatusPacket {
@@ -36,19 +35,19 @@ object SystemMonitor{
     }
 
     fun initMonitor(){
-        CoroutineScope(Dispatchers.IO).launch {
-            Tsk = async {
-                Timer().schedule(0L, Interval * 1000L) {
-                    launch { MonitorFlow.emit(getCurrentStatus()) }
-                }
-            }.await()
+        isActive = true
+        CoroutineScope(SupervisorJob() + Dispatchers.Default).launch {
+            while(isActive){
+                MonitorFlow.emit(getCurrentStatus())
+                delay(Interval.toLong() * 1000)
+            }
         }
     }
 
     fun getStatus(): StateFlow<StatusPacket> = MonitorFlow.asStateFlow()
 
     fun close(){
-        Tsk.cancel()
+        isActive = false
     }
 }
 
