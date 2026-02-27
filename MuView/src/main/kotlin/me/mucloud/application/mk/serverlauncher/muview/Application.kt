d1123b3a4e22b643cfcca8f9b501106bcbfc99d6
Mu_Cloud <Mu_Cloud@outlook.de>
2026-02-27 21:47:26 +0800

    [MuView] Remove Socket Plugin | 移除 Socket 插件

--- MuView/src/main/kotlin/me/mucloud/application/mk/serverlauncher/muview/Application.kt
+++ MuView/src/main/kotlin/me/mucloud/application/mk/serverlauncher/muview/Application.kt
@@ -2,20 +2,17 @@ package me.mucloud.application.mk.serverlauncher.muview
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import io.ktor.http.HttpMethod
-import io.ktor.serialization.gson.GsonWebsocketContentConverter
-import io.ktor.serialization.gson.gson
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.Sessions
-import io.ktor.server.websocket.WebSockets
-import io.ktor.server.websocket.pingPeriod
-import io.ktor.server.websocket.timeout
+import io.ktor.server.sessions.*
+import io.ktor.server.websocket.*
 import me.mucloud.application.mk.serverlauncher.MuCoreMini
 import me.mucloud.application.mk.serverlauncher.muenv.JavaEnvironment
 import me.mucloud.application.mk.serverlauncher.muenv.JavaEnvironmentAdapter
@@ -23,7 +20,6 @@ import me.mucloud.application.mk.serverlauncher.muserver.MCJEServer
 import me.mucloud.application.mk.serverlauncher.muserver.MCJEServerAdapter
 import me.mucloud.application.mk.serverlauncher.muserver.MCJEServerType
 import me.mucloud.application.mk.serverlauncher.muserver.MCJEServerTypeSerializer
-import me.mucloud.application.mk.serverlauncher.muview.mulink.initSocket
 import me.mucloud.application.mk.serverlauncher.muview.mulink.initWebSocket
 import me.mucloud.application.mk.serverlauncher.muview.session.initMuSessionManager
 import me.mucloud.application.mk.serverlauncher.muview.view.initEnvRoute
@@ -41,7 +37,9 @@ val gson: Gson = GsonBuilder()
 
 fun main() {
     MuCore.start()
-    embeddedServer(Netty, port = 20038, module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = MuCore.getMuCoreConfig().muCorePort, module = Application::module)
+        .start(wait = true)
+        .addShutdownHook(MuCore::stop)
 }
 
 fun Application.installPlugins(){
@@ -80,7 +78,6 @@ fun Application.module() {
     initServerRoute()
     initEnvRoute()
     initWebSocket()
-    initSocket()
     initMuView()
 }
 
